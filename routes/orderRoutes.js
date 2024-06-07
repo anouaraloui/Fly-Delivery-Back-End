628690714549b7468b55b8c89a1dac4cc31b4bc0
anouaraloui <alouianwer21@gmail.com>
2024-06-07 14:25:50 +0100

    refactor(order): remove role (Restaurant and Deliveryman) from route for add new order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -7,7 +7,7 @@ import checkArticle from "../middlewares/checkArticle.js";
 const router = express.Router();
 
 // Route for add new order
-router.post('/order/:id', isAuth, (req, res, next) => role(['Admin', 'Restaurant', 'Deliveryman', 'Customer'], req, res, next),
+router.post('/order/:id', isAuth, (req, res, next) => role(['Admin', 'Customer'], req, res, next),
 checkArticle, addNewOrderController);
 
 export default router;
