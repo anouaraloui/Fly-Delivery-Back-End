7af9c7379105105a7a094f6021a534ed8ec37f95
anouaraloui <alouianwer21@gmail.com>
2024-06-21 01:55:15 +0100

    feat(deliveryman): add service, controller and route to make a decision on the order

--- controllers/deliveryManController.js
+++ controllers/deliveryManController.js
@@ -1,8 +1,18 @@
-import { getAllOrdersAvailable } from "../services/deliverymanService.js";
+import { decisionOrder, getAllOrdersAvailable } from "../services/deliverymanService.js";
+import findUserId from "../utils/findUserId.js";
 
 
 // Controller for display all orders available
 export const getAllOrdersAvailableControlelr = async (req, res) => {
     const getAllOrdersAvailableService = await getAllOrdersAvailable(req.query);
     return res.status(getAllOrdersAvailableService.status).json({ response: getAllOrdersAvailableService });
+};
+
+// Controller for accept or reject an order
+export const decisionOrderController = async (req, res) => {
+    const { id } = req.params;
+    const userId = ((await findUserId(req)).userId);
+    const decision = req.body.decision;
+    const decisionOrderService = await decisionOrder(id, userId, decision);
+    return res.status(decisionOrderService.status).json({ response: decisionOrderService });
 };
--- services/deliverymanService.js
+++ services/deliverymanService.js
@@ -7,6 +7,7 @@ export const getAllOrdersAvailable = async (data) => {
         if (!data.limit) data.limit = 30;
         const skipPage = (data.page - 1) * data.limit;
         const ordersList = await Order.find().where('restaurantStatus').equals(true)
+                            .$where('this.deliverymanStatus != true')
                             .skip(skipPage)
                             .limit(parseInt(data.limit))
                             .exec();
@@ -16,4 +17,24 @@ export const getAllOrdersAvailable = async (data) => {
         } catch (error) {
             return { status: 400, success: false, message: error.message };
         };
+};
+
+// Service to accept or reject an order
+export const decisionOrder = async (id, userId, decision) => {
+    return await Order.findById(id).where('restaurantStatus').equals(true).$where('this.deliverymanStatus != true')
+    .then( async (order) => {
+        if(!order) return { status: 404, succes: false, message: 'There are no orders available yet' };
+        await Order.findByIdAndUpdate(
+            {_id: id},
+            {
+                $set: {
+                    "deliverymanId": userId,
+                    "deliverymanStatus": decision
+                }
+            }
+        );        
+        return { status: 200, succes: true, message: "Your answer is succussffully send" };
+    }).catch((err) => {
+        return { status: 400, succes: false, message: err.message }; 
+    });
 };
